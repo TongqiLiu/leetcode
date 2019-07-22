a893d619a980ad0b3191b613132bc1b0680c7b4b
tongqi.ltq <tongqi.ltq@alibaba-inc.com>
2019-07-22 21:45:07 +0800

    SearchInRotatedSortedArray题解

---
+++ Algorithms/Java/src/SearchInRotatedSortedArray/SearchInRotatedSortedArray.java
@@ -0,0 +1,50 @@
+package src.SearchInRotatedSortedArray;
+
+/**
+ * @author mingqiao
+ * @Date 2019/7/22
+ */
+public class SearchInRotatedSortedArray {
+
+    /**
+     * 核心思路就是二分，复杂度O(logN)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int search(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < nums[right]) {
+                if (nums[mid] < target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (nums[left] <= target && nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(search(nums, 0));
+    }
+}
