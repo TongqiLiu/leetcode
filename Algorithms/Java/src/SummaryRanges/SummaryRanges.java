35f81b63e8472c8dcd721266a26c09b6554ae2c1
tongqi <tongqicode@126.com>
2025-12-28 11:52:37 +0800

    添加SummaryRanges: 汇总有序数组中的连续数字范围

---
+++ Algorithms/Java/src/SummaryRanges/SummaryRanges.java
@@ -0,0 +1,22 @@
+package src.SummaryRanges;
+
+import java.util.List;
+
+/**
+ * @author tongqi
+ * @date 2025/12/28
+ */
+public class SummaryRanges {
+
+    /**
+     * 将有序数组中的连续数字范围汇总为区间列表
+     * 
+     * @param nums 有序数组
+     * @return 区间列表，格式为 ["a->b", "c"] 等
+     */
+    public List<String> summaryRanges(int[] nums) {
+        // TODO: 实现汇总区间的逻辑
+        return null;
+    }
+}
+
